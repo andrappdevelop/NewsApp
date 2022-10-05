6c46e1ddee13af5323277e3b5689fc12d73b0be3
Ivan Usatov <andrappdevelop@gmail.com>
2022-10-05 17:27:19 +0700

    Chenged

--- app/src/main/java/com/example/newsapp/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.newsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-//asd
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
